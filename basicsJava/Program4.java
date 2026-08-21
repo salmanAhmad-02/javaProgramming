ea228098c5d15be6b64761cecef6de2e6e54e4f6
salmanAhmad-02 <salman9838414103@gmail.com>
2026-08-21 21:59:36 +0530

    String Methods

--- basicsJava/Program4.java
+++ basicsJava/Program4.java
@@ -1,3 +1,14 @@
 class Program4{
+	public static void main(String[] args){
+		//Some important methods and featues of String class
+		String s1="Salman";
+		String s2="salman";
+		
+		//equals()
+		System.out.println(s1.equals(s2));
+
+		//equalsIgnoreCase()
+		System.out.println(s1.equalsIgnoreCase(s2));
+	}
 	
 }
