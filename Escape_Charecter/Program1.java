58e181acdcb296dda893674346486428f5421fce
salmanAhmad-02 <salman9838414103@gmail.com>
2026-08-24 17:32:04 +0530

    Escape Charecter

--- Escape_Charecter/Program1.java
+++ Escape_Charecter/Program1.java
@@ -6,7 +6,16 @@ class Program1{
 		System.out.println("'");
 		System.out.println('\'');
 
-		//\t- to generate a tab sapce
+		//-\t- to generate a tab sapce
 		System.out.println("Today is\t Monday");
+
+		//-\n- to bring cursor to new line 
+		System.out.println("Here is \nSalman Ahmad");
+
+		/* You can't put anything afetr \ , like 
+		System.out.println("John and \Jack\ are here"); will raise an error , illegal Escape charecter */
+		
+		//if you want to use \ into your output console then you must do like this (use one more \ before anfer that \) 
+		System.out.println("John and \\Jack\\ are here");
 	}
 }
