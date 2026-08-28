e2707777cc4258694bae8008bc5c473a293112f9
salmanAhmad-02 <salman9838414103@gmail.com>
2026-08-28 23:57:57 +0530

    charecters ASCII values

---
+++ charecter/Program2.java
@@ -0,0 +1,10 @@
+class Program2{
+	public static void main(String[] args){
+		System.out.println((char)45);
+		System.out.println((char)33);
+		System.out.println((char)188);
+		System.out.println((char)1880);
+		System.out.println('*'+0);
+		System.out.println((int)'*');
+	}
+}
