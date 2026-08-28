eda5f96f67ac539b1b902f51a1d318d5a8692013
salmanAhmad-02 <salman9838414103@gmail.com>
2026-08-29 00:14:58 +0530

    charecter operations

---
+++ charecter/Program1.java
@@ -0,0 +1,15 @@
+class Program1{
+	public static void main(String[] args){
+		System.out.println('a'+10);
+		System.out.println('@');
+		System.out.println('@'+10);
+		System.out.println('@'-10);
+		System.out.println('@'+'d');
+		System.out.println(2+2);
+		System.out.println('2'+2);
+		System.out.println("2"+2);
+		System.out.println('2'+'2');
+		System.out.println('R'-'r');
+		
+	}
+}
