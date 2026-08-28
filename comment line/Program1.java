dbdae41abdc3db9b5d602c2c9496629e2af7dcc3
salmanAhmad-02 <salman9838414103@gmail.com>
2026-08-28 23:32:22 +0530

    Comment line in java

---
+++ comment line/Program1.java
@@ -0,0 +1,36 @@
+//This program is to understand escape characters
+
+/*
+*This is Multiline comment
+*for escape
+*characters
+*
+*/
+
+
+/**
+*This class
+*Program1 is 
+*designed to understand
+*Escape characters
+*/
+public class Program1{
+
+	/**
+	*This method
+	*main is 
+	*designed to understand
+	*Escape characters
+	*/
+	public static void main(String[] args){
+		System.out.println("\"Mohan and \"Sohan\" are here\"");
+		System.out.println("\"");
+		System.out.println('"');
+		System.out.println("'");
+		System.out.println('\'');
+		System.out.println("Today is Monday");
+		System.out.println("Today is \tMonday");
+		System.out.println("Mohan and \nSohan are \nhere");
+		System.out.println("John and \\Jack\\ are here");
+	}
+}
