b092ee7d5374dd54fc0cc09427c5f15bf5403482
salmanAhmad-02 <salman9838414103@gmail.com>
2026-08-28 23:42:44 +0530

    Concatenation Learned

---
+++ concatenation/Program1.java
@@ -0,0 +1,11 @@
+class Program1{
+	public static void main(String[] args){
+		System.out.println("abc"+10+20);
+		System.out.println(10+20+"abc");
+		System.out.println(20+30+"abc"+10.6+20);
+		System.out.println('@'+"xyz");
+		System.out.println("xyz"+'a');
+
+
+	}
+}
