b4acf468a54dc0ba59c3d1d4d7357222f86f8a0f
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-01 23:24:34 +0530

    Array , length property and for Each loop

---
+++ String[ ]/Program2.java
@@ -0,0 +1,11 @@
+class Program2{
+	public static void main(String[] args){
+		System.out.println("Program starts");
+		int[] a={10, 12, 15, 16, 11, 14};
+		System.out.println("Array Size is: :"+a.length);
+		for(int n:a){
+			System.out.println(n);
+		}
+		System.out.println("Program Ends");
+	}
+}
