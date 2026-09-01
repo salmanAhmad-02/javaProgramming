fee61918f3ecb8d872c40067c75b7ebab9f4e8dc
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-01 23:25:52 +0530

    String type Array

---
+++ String[ ]/Program3.java
@@ -0,0 +1,12 @@
+class Program3{
+	public static void main(String[] args){
+		System.out.println("Program starts");
+		String[] names={"John", "Jack", "Sohan", "Mohan"};
+		System.out.println("Array Size is: "+names.length);
+		for(String name:names){
+			System.out.println("Name is: "+name);
+		}
+
+		System.out.println("Program Ends");
+	}
+}
