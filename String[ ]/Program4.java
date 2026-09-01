f20ecff0b566924d68c66d75a8360ec293c0cf92
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-01 23:30:34 +0530

    Understanding String[] args

---
+++ String[ ]/Program4.java
@@ -0,0 +1,10 @@
+class Program4{
+	public static void main(String[] args){
+		System.out.println("Program starts");
+		System.out.println("Array Size is: "+args.length);
+		for(String s:args){
+			System.out.println(s);
+		}
+		System.out.println("Program Ends");
+	}
+}
