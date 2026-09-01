3b5a0af8050251b0b7ba244696d8aeecbb2d132a
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-01 23:22:27 +0530

    Understandin Basics of int type Array

---
+++ String[ ]/Program1.java
@@ -0,0 +1,12 @@
+class Program1{
+	public static void main(String[] args){
+		System.out.println("Program starts");
+		int[] a={10, 12, 15, 16, 11, 14};
+		System.out.println(a);
+		System.out.println(a.length);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[a.length-1]);
+		System.out.println("Program Ends");
+	}
+}
