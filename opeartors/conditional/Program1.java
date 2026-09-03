5d87a1e75c561f085d2b3ac7278722d7afadaffd
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-03 23:23:43 +0530

    Program to check n is 3-digit or NOT

---
+++ opeartors/conditional/Program1.java
@@ -0,0 +1,11 @@
+//Program to check n is 3-digit or NOT
+class Program1{
+	public static void main(String[] args){
+		int n=  1885 ;
+
+	String res=(n>=100 && n<=999) ? n+" is a 3-digit Number":n+" is a NOT a 3-digit Number";
+
+		System.out.println(res);
+		
+	}
+}
