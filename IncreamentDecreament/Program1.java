32673f7a54df0795dfd5f69ed65c227ad2da2579
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-04 20:40:57 +0530

    Increament/Decreament Opearations

---
+++ IncreamentDecreament/Program1.java
@@ -0,0 +1,13 @@
+class Program1{
+	public static void main(String[] args){
+		int a=10;
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(--a);
+		System.out.println(a--);
+		System.out.println(++a);
+		System.out.println(a++);
+		System.out.println(++a);	
+	}
+}
