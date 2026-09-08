b32e11e03c7b4bbd118841f01604f2698d9cead8
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-08 23:57:43 +0530

    Program to convert number into absolute without inbuilt feature

---
+++ opeartors/conditional/Program5.java
@@ -0,0 +1,8 @@
+//Program to convert number into absolute valuse
+class Program5{
+	public static void main(String[] args){
+		int num=76;
+		int absValue=(num>0)?num:-num;
+		System.out.println("absoulute value for "+num+" is :"+absValue);
+	}
+}
