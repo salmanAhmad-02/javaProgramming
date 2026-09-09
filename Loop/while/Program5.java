b1ca6fb499f2f89ee20bb850f7a060901ec4b2b0
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-09 23:17:09 +0530

    Value of variable outside of loop

---
+++ Loop/while/Program5.java
@@ -0,0 +1,13 @@
+class Program5{
+	public static void main(String[] args){
+		int i=1;
+		while(i<=10){
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("i outside loop is: "+i); 
+		/* Will print value of is 11 because increament of i is done inside , and i becomes 11 , and condition fails and got out the loop */
+
+		
+	}
+}
