97e54c102bfbba4ea9845981276d4933e2c58de9
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-09 23:01:58 +0530

    Numeric Literals In Diffrent Number Bases

---
+++ Loop/while/Program1.java
@@ -0,0 +1,24 @@
+class Program1{
+	public static void main(String[] args){
+
+		//decimal number (start with 1 to 9)
+		System.out.println(322);
+
+		//binary base 2 (start with 0b or 0B)
+		System.out.println(0b10101);
+
+		//octal base 8 (start with 0)
+		System.out.println(0322);
+
+		//hexadecimal base 16 (start with 0x or 0X)
+		System.out.println(0x322);
+
+
+		int i=1;
+		while(i<=10){
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+}
