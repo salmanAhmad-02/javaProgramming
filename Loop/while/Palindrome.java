0f4f2e9d9d855bec4a6ef747d648c728b7404f3d
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-12 15:58:23 +0530

    Added Method to check Palindrome for a String input

--- Loop/while/Palindrome.java
+++ Loop/while/Palindrome.java
@@ -2,17 +2,33 @@ class Palindrome{
 	public static void main(String[] args){
 		int num1=13531;
 		
-		if(isPalindrome(num1))
-			System.out.println(num1+" is Palindrome");
+		if(isPalindromeNumber(num1))
+			System.out.println("'" + num1 + "' is Palindrome Number");
 		else
-			System.out.println(num1+" is not Palindrome");
+			System.out.println("'" + num1 + "' is not Palindrome Number");
+		
+		String s1="madam";
+		if(isPalindromeString(s1))
+			System.out.println("\"" + s1 + "\" is Palindrome String");
+		else
+			System.out.println("\"" + s1 + "\" is not Palindrome String");
 	}
-	public static boolean isPalindrome(int x){
+	public static boolean isPalindromeNumber(int x){
 		int rev=0;	int n=x;
 		while(x>0){
 			rev=rev*10+(x%10);
 			x /=10;
 		}
 		return n==rev;
+	}
+	public static boolean isPalindromeString(String s){
+		if (s == null) return false;
+		String revString="";
+		int i = s.length() - 1;
+		while (i >= 0) {
+			revString +=s.charAt(i);
+			i--;
+		}
+		return s.equals(revString);
 	}
 }
