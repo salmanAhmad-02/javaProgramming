9e5cd998ea1811bbd90267ed825144d65c494936
salmanAhmad-02 <salman9838414103@gmail.com>
2026-09-24 22:08:20 +0530

    Added Method to get Nth Term, and Handled Negative Inputs

--- DSA/array/FibonacciSeries.java
+++ DSA/array/FibonacciSeries.java
@@ -1,24 +1,45 @@
 class FibonacciSeries{
     public static int[] generateFibonacci(int n){
+        if(n<0){
+            return new int[0];
+        }
         int[] fibo=new int[n];
-        if(n==0)    return fibo;
-        fibo[0]=0;
-        if(n==1)    return fibo;
-        fibo[1]=1;
+        if(n>=0)    fibo[0]=0;
+        if(n>=1)    fibo[1]=1;
         for(int i=2; i<n; i++){
             fibo[i]=fibo[i-2]+fibo[i-1];
         }
         return fibo;
     }
-    public static void printFibonacciSeries(int n){
+    public static void printNFibonacciSeries(int n){
+        if(n<0){
+            System.out.println("Invalid Argument: "+n+" (Index cannot be negative)");
+            return;
+        }
         int[] series=generateFibonacci(n);
         System.out.println("Fibonacci Series of "+n+" terms are : ");
         for(int i=0; i<series.length; i++){
             System.out.print(series[i]+" ");
         }
+        System.out.println("\n");
+    }
+    public static int getNthFibonacciTerm(int n){
+        if(n<0){
+            return -1;
+        }
+        int[] allTerms=generateFibonacci(n);
+        return allTerms[allTerms.length-1];
     }
     public static void main(String[] args){
-        int n=8;
-        printFibonacciSeries(n);
+        int terms=10;
+        printNFibonacciSeries(terms);
+
+        int nthTermValue=getNthFibonacciTerm(terms);
+        if(nthTermValue<0){
+            System.out.println("Invalid Argument or Negative "+terms);
+        }
+        else{
+            System.out.println(terms+"th Term is : "+nthTermValue);
+        }
     }
 }
